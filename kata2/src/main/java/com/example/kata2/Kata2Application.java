eebbc8b7ce68c98d5144edd4e49b6ab48572d449
inescortina <114484056+inescortina@users.noreply.github.com>
2023-07-25 15:52:22 +0100

    Add files via upload

---
+++ kata2/src/main/java/com/example/kata2/Kata2Application.java
@@ -0,0 +1,13 @@
+package com.example.kata2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Kata2Application.class, args);
+  }
+
+}
---
+++ kata2/src/test/java/com/example/kata2/Kata2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kata2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
