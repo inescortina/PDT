17cdcc01be2c7a85884bb2241abca24fe036065b
inescortina <114484056+inescortina@users.noreply.github.com>
2023-07-25 15:53:08 +0100

    Add files via upload

---
+++ kata03/src/main/java/com/example/kata03/Kata03Application.java
@@ -0,0 +1,13 @@
+package com.example.kata03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata03Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Kata03Application.class, args);
+  }
+
+}
---
+++ kata03/src/test/java/com/example/kata03/Kata03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kata03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata03ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
